4e64c3aa2e189a172e694c19b1611bfab55a9bda
BilhaqAD07 <87814270+BilhaqAD07@users.noreply.github.com>
2023-11-27 14:51:44 +0700

    update(fix): fix blogpost page display

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -3,15 +3,15 @@ import React from 'react'
 
 const Post = () => {
   return (
-    <div className='bg-black/30 h-full w-full relative'>
-      <div className="container mx-auto">
-        <div className="top flex flex-row items-center gap-8">
+    <div className='bg-black/30 h-full w-full overflow-y-scroll relative pb-8'>
+      <div className="container mx-auto ">
+        <div className="top flex md:flex-row flex-col-reverse md:gap-4 md:mt-8">
           {/* Header Content */}
-          <div className="info flex flex-1 justify-between flex-col gap-4">
+          <div className="info flex flex-1 justify-center flex-col gap-4">
             <h2 className='title text-xl md:text-4xl'>
               Ini Judul
             </h2>
-            <p className='desc'>
+            <p className='desc text-sm md:text-base font-light'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis consectetur nulla, at quam aliquid placeat, perspiciatis nam vero exercitationem deserunt obcaecati modi aliquam voluptas fugit laborum architecto quaerat officiis commodi.
             </p>
             <div className="author relative flex items-center gap-3">
@@ -27,7 +27,7 @@ const Post = () => {
               </span>
             </div>
           </div>
-          <div className="image-container flex flex-1 h-[300px] relative">
+          <div className="image-container flex md:flex-1 h-[300px] relative">
             <Image
               src={'https://images.pexels.com/photos/19146694/pexels-photo-19146694/free-photo-of-a-red-maple-tree-with-leaves-on-it.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'}
               alt='blog-image'
@@ -37,9 +37,15 @@ const Post = () => {
           </div>
         </div>
         {/* Content */}
-        <div className="content">
+        <div className="content mt-12 font-light text-justify text-sm md:text-base">
           <p className="text">
-
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore dicta aut rem similique deserunt aspernatur, a fugit voluptatem, expedita, repudiandae eius beatae. Et nesciunt molestiae adipisci, esse blanditiis corporis modi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, rem iste! Accusantium quidem harum ut vitae commodi earum molestias odit nobis culpa, corrupti dolorem dolorum, nisi dicta totam eius adipisci!
+            <br />
+            <br />
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore itaque mollitia exercitationem voluptatum nemo modi, assumenda fuga molestiae maiores, culpa quis delectus eius reprehenderit quod nesciunt consequatur error architecto! Repellendus.
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat est perspiciatis atque expedita nam ad impedit incidunt aperiam? Dolorum exercitationem autem magnam officia sit unde iusto suscipit ut accusantium deleniti.
           </p>
         </div>
       </div>
