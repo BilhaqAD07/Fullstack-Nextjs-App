95d16af07e897048d58e02cfa11cf8d9ce59d0b5
BilhaqAD07 <87814270+BilhaqAD07@users.noreply.github.com>
2023-11-28 15:03:10 +0700

    update(feat): build darkmode toggle as component

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import DarkModeToggle from '../DarkModeToggle/DarkModeToggle'
 import { useEffect, useState } from 'react'
 import {
     FaBars,
@@ -90,7 +91,10 @@ export default function Navbar () {
                     <button className='btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group'>
                         <span className='group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500'>Logout</span>
                         <BiLogOut className='-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]'/>
-                </button>
+                    </button>
+                  </li>
+                  <li className=''>
+                    <DarkModeToggle/>
                   </li>
               </ul>
           </div>
